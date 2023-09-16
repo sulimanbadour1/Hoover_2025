ab36fbe04e7817d77568cd68b3b78df03fbdbef7
Anna <annakopy@centrum.cz>
2023-09-16 17:51:54 +0200

    SensorControlApp/source
    RPLidarWindows.py rewritten to multiwindow concept.
    
    Fixing Previous commit
    In previous commit the info "fixing DeviceTemplates' bugs" is wrong.
    It was fixing WindowsTemplates.py not DeviceTemplates.py

--- RPLidarWindows.py
+++ RPLidarWindows.py
@@ -1,233 +1,176 @@
 import sys
 import time
 from math import sin, cos
-from DeviceTemplate import DeviceTemplate
-from WindowsTemplates import ControlTemplate, PlotTemplate, TerminalTemplate
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread, QObject
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QLabel, QVBoxLayout, QAction, QMenuBar, QMenu
-from adafruit_rplidar_AK import RPLidar, RPLidarException
+from device_interfaces.RPLidarInterface import Device
+from WindowsTemplates import DeviceWindowTemplate, ControlTemplate, PlotTemplate, TerminalTemplate, InfoWindowTemplate
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread, QObject, Qt
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QSlider, QVBoxLayout, QHBoxLayout, QAction, QLineEdit
 from pyqtgraph import  plot, mkPen, PlotItem, ScatterPlotWidget, ScatterPlotItem, mkBrush
 import pyqtgraph as pg
 
-class Device(DeviceTemplate):
-    data_signal =pyqtSignal(tuple)
-    end_thread_signal = pyqtSignal(bool)
-    message_signal =  pyqtSignal(str)
-    info_signal = pyqtSignal(dict)
+class RPLidarWindow(DeviceWindowTemplate):
 
-    def __init__(self, port):
-        super().__init__(self)
-        self.port = port
-        self.device = RPLidar(None, port, timeout=3)
-        self.device.stop_motor()
-        self.data_state = True
-        self.motor_running = False
-
-    
-    def startMotor(self):
-        if self.motor_running == False:
-            self.device.start_motor()
-            self.motor_running = True
-        else:
-            message = "Motor already started"
-            self.message_signal.emit(message)
-
-    def stopMotor(self):
-
-        if self.device_started == True:
-            self.stopDevice()
-        if self.motor_running == True:
-            self.device.stop_motor()
-            self.motor_running = False
-        else:
-            message = "Motor already stopped"
-            self.message_signal.emit(message)
-        
-
-    def startDevice(self):
-        try:
-            self.device.start()
-            self.device_started = True
-        except RPLidarException:
-            print("RPLidarException: Device already started.")
-
-    def stopDevice(self):
-        try:
-            self.end_thread_signal.emit(True) #signal to get out of the thread in case the measure_data function has been called
-            self.device.stop()
-            self.device_started = False
-        except RPLidarException:
-            print("RPLidarException: Device already stopped.")
-
-    @pyqtSlot()
-    def measureData(self):
-        """
-        Function for data receive.
-        Function generates signal emiting the tuple 'data'. 
-        In case termination of function is required it is necessary to terminate the whole thread (use 'end_thread_signal').
-        """
-        self.device_running = True
-        self.motor_running = True
-        message = "Measuring state"
-        self.message_signal.emit(message)
-        while True:
-            angle = [0]
-            distance = [0]
-            time.sleep(0.05)
-            #data = self.device.once_measurements()
-            try:
-                iterator = self.device.iter_scans()
-                scan = next(iterator)
-                for item in scan:
-                    angle.append(item[1])
-                    distance.append(item[2])
-                data = (angle, distance)
-            except RPLidarException:
-
-                #UnboundLocalError: local variable 'data' referenced before assignment
-                #in case there is a mistake in data it is necessary to send some data
-                data = ([0],[0])
-                #Exception for 'Wrong body size' or 'Incorrect descriptor starting bytes' 
-                #Device becomes uncontrollable .
-                #Solution is reconnection or physical disconnection
-
-                message = """'
-                Wrong body size' or 'Incorrect descriptor starting bytes'. 
-                Please press 'Stop', -> 'Start' -> 'Measure'"""
-                self.message_signal.emit(message)
-    
-
-            self.data_signal.emit(data)
-
-    def reconnectDevice(self):
-
-        #Function enables renewing of the connection
-        self.device.disconnect()
-        self.device_started = False
-        self.device = RPLidar(None, self.port, timeout=3)
-        self.startDevice()
-        self.device.stop_motor()
-
-    def resetDevice(self):
-        self.device.reset() #resets device to idle
-        self.reconnectDevice() #creates connection again
-
-    def getDeviceInfo(self):
-        info_message = self.device.info
-        self.info_signal.emit(info_message)
-
-class RPLidarWindow(ControlTemplate):
-    trigger_measure_signal = pyqtSignal(bool)
-
-    def __init__(self, port):
+    def __init__(self, parent, port = 'COM3'):
         super().__init__()
-        self.setWindowTitle("Control Panel - RP Lidar")
-        self.setGeometry(100, 100, 300, 500)
-        
         self.port = port
-        self.device_control = Device(self.port)
-        self.communication_thread = QThread()
-        self.terminal = TerminalWindow(self)
-        self.plot_window = PlotWindow(self)
-        self.info_terminal = InfoTerminal(self)
-        self.createGUI()
-        self.adjustGUI()
-        self.createMenu()
-        self.createStatusBar()
-        self.connectGUI()
-        self.create_thread_communication()
-    
-      
 
+        self.control_window = ControlPanelWindow(self)
+        self.terminal_window = TerminalWindow(self)
+        self.plot_window = PlotWindow(self)
+        self.info_window = InfoWindow(self)
+        self.device_interface = Device(self, self.port)
+        self.device_thread = QThread(self)
+        self.createGUI()
+        self.connectGUI()
+        self.createThreadCommunication()
+
+    def createGUI(self):
+        return super().createGUI()
+    
+    def connectGUI(self):
+        return super().connectGUI()
+    
     def adjustGUI(self):
+        #Creating menu options for dosconnection and reconnection of the device
+        self.disconnect_device = QAction("&Disconnect Device")
+        self.reconnect_device = QAction("&Reconnect Device")
+        self.device_menu.addAction(self.disconnect_device)
+        self.device_menu.addAction(self.reconnect_device)
+    
+  
+    def connectElements(self):
+
+        #Connecting buttons to functions
+        self.control_window.start_button.clicked.connect(self.device_interface.startDevice)
+        self.control_window.stop_button.clicked.connect(self.device_interface.stopDevice)
+        self.control_window.start_motor_button.clicked.connect(self.device_interface.startMotor)
+        self.control_window.stop_motor_button.clicked.connect(self.device_interface.stopMotor)
+        self.control_window.start_measure_button.clicked.connect(self.device_interface.measureData)
+        self.device_info.triggered.connect(self.device_interface.getDeviceInfo)
+        self.reconnect_device.triggered.connect(self.device_interface.reconnectDevice)
+        self.disconnect_device.triggered.connect(self.device_interface.disconnectDevice)
+
+
+        #Connecting signals from device control to functions (slots)
+        self.device_interface.data_signal.connect(self.terminal_window.receiveData)
+        self.device_interface.data_signal.connect(self.plot_window.updatePlot)
+        self.device_interface.info_signal.connect(self.info_window.receiveData)
+        self.device_interface.message_signal.connect(self.setStatusBarText)
+        self.control_window.pwm_value_signal.connect(self.device_interface.setPWM)
+
+class ControlPanelWindow(ControlTemplate):
+    trigger_measure_signal = pyqtSignal(bool)
+    pwm_value_signal = pyqtSignal(int)
+    message_signal = pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.createGUI()
+        self.connectGUI()
+
+    def createGUI(self):
         """
         Function creates graphical user interface GUI
         """
+        #window properties
+        self.setWindowTitle("Control Panel - RP Lidar")
+        self.setGeometry(100, 100, 300, 500)
+
+        #Creating layouts snd sublayouts
+        self.layout = QHBoxLayout()
+        self.button_layout = QVBoxLayout()
+        self.slider_layout = QVBoxLayout()
+        self.button_layout_widget = QWidget()
+        self.slider_layout_widget = QWidget()
+
+        #Creating elements and setting the geometry
         self.start_button = QPushButton("Start")
         self.stop_button = QPushButton("Stop")
         self.start_motor_button = QPushButton("Start motor")
         self.stop_motor_button = QPushButton("Stop motor")
         self.start_measure_button = QPushButton("Measure")
-        self.reset_button = QPushButton("Reset")
         self.start_button.setGeometry(50, 50, 200, 25)
         self.stop_button.setGeometry(50, 100, 200, 25)
         self.start_motor_button.setGeometry(50, 150, 200, 25)
         self.stop_motor_button.setGeometry(50, 200, 200, 25)
-        self.reset_button.setGeometry(50, 200, 200, 25)
+        self.slider = QSlider(self)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(1000)
+        self.slider.setMinimumHeight(200)
+        self.slider.setMaximumHeight(1000)
+        self.pwm_label = QLabel("PWM: ")
+        self.pwm_input = QLineEdit(self)
+        
     
-        self.layout.addWidget(self.start_button)
-        self.layout.addWidget(self.stop_button)
-        self.layout.addWidget(self.start_motor_button)
-        self.layout.addWidget(self.stop_motor_button)
-        self.layout.addWidget(self.start_measure_button)
-        self.layout.addWidget(self.reset_button)
+        #Adding elemts to the layouts
+        self.button_layout.addWidget(self.start_button)
+        self.button_layout.addWidget(self.stop_button)
+        self.button_layout.addWidget(self.start_motor_button)
+        self.button_layout.addWidget(self.stop_motor_button)
+        self.button_layout.addWidget(self.start_measure_button)
+        self.slider_layout.addWidget(self.pwm_label, alignment= Qt.AlignCenter)
+        self.slider_layout.addWidget(self.slider, alignment= Qt.AlignCenter )
+        self.slider_layout.addWidget(self.pwm_input, alignment= Qt.AlignCenter)
+        
+
+        self.button_layout_widget.setLayout(self.button_layout)
+        self.slider_layout_widget.setLayout(self.slider_layout)
+
+        #Adding sublayouts to the main layout
+        self.layout.addWidget(self.button_layout_widget)
+        self.layout.addWidget(self.slider_layout_widget)
 
         self.setLayout(self.layout)
 
-
-    
     def connectGUI(self):
-        """
-        Function creates connection between elements in the GUI.
-        """
+        self.slider.valueChanged.connect(self.changePWMInput)
+        self.slider.sliderReleased.connect(self.emitPWMvalueSlider)
+        self.pwm_input.editingFinished.connect(self.emitPWMvalueInput)
 
-        self.show_terminal.triggered.connect(self.showTerminalWindow)
-        self.show_graphics.triggered.connect(self.showGraphicWindow)
-        self.device_info.triggered.connect(self.showDeviceInfoWindow)
-        self.show_terminal.triggered.connect(self.showTerminalWindow)
-        self.device_control.end_thread_signal.connect(self.reinitialize_thread)
+    
+    def changePWMInput(self, value: int):
+        """
+        Changes value in the input field when the to correspond with the actual value on slider.
+        It must be connected to the function self.slider.valueChanged.connect(self.changePWMInput) 
+        while it must be updated even when the slider is changed continuous.
+        """
+        pwm_value = self.slider.value()
+        self.pwm_input.setText(str(pwm_value))
+    
+    def changePWMSlider(self, value: int):
+        """
+        Changes value of the slider when the to correspond with the actual value inserted in input field.
+        It is called in the function self.editPWMvalueInput while it changes only after pressing enter.
+        """
+        self.slider.setValue(value)
+       
+
+    def emitPWMvalueSlider(self):
+        pwm_value = self.slider.value()
+        print(type(pwm_value))
+        self.pwm_value_signal.emit(pwm_value)
+
+    def emitPWMvalueInput(self):
         
-        #Connecting signals from device control to functions (slots)
-        self.device_control.data_signal.connect(self.terminal.receiveData)
-        self.device_control.data_signal.connect(self.plot_window.updatePlot)
-        self.device_control.message_signal.connect(self.receiveMessage)
-        self.device_control.info_signal.connect(self.info_terminal.receiveData)
-
-        #Connecting buttons to functions
-        self.start_button.clicked.connect(self.device_control.startDevice)
-        self.stop_button.clicked.connect(self.device_control.stopDevice)
-        self.start_motor_button.clicked.connect(self.device_control.startMotor)
-        self.stop_motor_button.clicked.connect(self.device_control.stopMotor)
-        self.start_measure_button.clicked.connect(self.device_control.measureData)
-        self.reset_button.clicked.connect(self.device_control.resetDevice)
+        try:
+            pwm_value = int(self.pwm_input.text())
+            self.pwm_value_signal.emit(pwm_value)
+            self.changePWMSlider(pwm_value)
+        except ValueError:
+            message = "Wrong Input Type"
+            self.message_signal.emit(message)
 
 
-    
-    def showTerminalWindow(self):
-        self.terminal.show()
-    
-    def showGraphicWindow(self):
-        self.plot_window.show()
 
-    def showDeviceInfoWindow(self):
-        self.device_control.getDeviceInfo()
-        self.info_terminal.show()
-    
-    def create_thread_communication(self):
-        """
-        Function moves the object self.device_control into this thread and starts the thread.
-        """
-        self.device_control.moveToThread(self.communication_thread)
-        self.communication_thread.start()
-    
-    def reinitialize_thread(self, state):
-        """
-        Function terminates the thread and starts it again.
-        """
-        if state == True:
-            self.communication_thread.terminate()
-            self.create_thread_communication()
-    
+        #self.pwm_value_signal.emit(pwm_value)
 
-            
-
+        
 
 
 class TerminalWindow(TerminalTemplate):
     def __init__(self, parent):
         super().__init__(parent)
-       
-
+        self.createGUI()
     
     @pyqtSlot(tuple)
     def receiveData(self, data):
@@ -245,26 +188,14 @@ class PlotWindow(PlotTemplate):
         self.x_values = [0]
         self.y_values = [0]
         
-        self.setWindowTitle("Plot")
-        self.setGeometry(700, 100, 800, 600)
         self.createGUI()
         
-    def createGUI(self):
-        self.createPlot()
+    def adjustGUI(self):
 
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.graph)
-        self.setLayout(self.layout)
-    
-    def createPlot(self):
-        self.graph = plot()
-        self.graph.setBackground('w')
-        self.graph_scatter = ScatterPlotItem(size = 10, brush=pg.mkBrush(30, 255, 0, 255) )
-        self.plot_data = self.graph_scatter.addPoints(self.x_values, self.y_values)
-        self.graph.addItem(self.graph_scatter)
+        #Adjjusting plot (plot in original template measures in the meters while RPLidar measures in the milimeters)
         self.graph.setXRange(-1500, 1500)
         self.graph.setYRange(-1500,1500)
-     
+          
     @pyqtSlot(tuple)
     def updatePlot(self, data):
 
@@ -283,12 +214,14 @@ class PlotWindow(PlotTemplate):
         self.plot_data = self.graph_scatter.addPoints(self.x_values, self.y_values )
 
   
-class InfoTerminal(TerminalWindow):
+class InfoWindow(InfoWindowTemplate):
     def __init__(self,parent):
         super().__init__(parent)
+        self.createGUI()
     
     @pyqtSlot(dict)
     def receiveData(self, data):
+         self.clearOutputBox()
          for key, value in data.items():
             text = "{}: {}\n".format(key, value)
             self.output_box.insertPlainText(text)
