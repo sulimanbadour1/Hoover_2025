b503389a77674e951c6ff36dd47cd003c2bc3b0c
Anna <annakopy@centrum.cz>
2023-09-24 23:59:23 +0200

    Sensor Control App/source
    MainWindow.py creating content.

--- MainWindow.py
+++ MainWindow.py
@@ -7,23 +7,21 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot, QObject
 from serial.tools.list_ports import comports
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import *
 from SettingsWindow import SettingsWindow
 from AppValues import AppValues
-from sens_app.WindowsTemplates import ConnectionInfoWindow, MainWindowContent
+#from sens_app.WindowsTemplates import ConnectionInfoWindow, MainWindowContent
 from RPLidarWindows import RPLidarWindow
 from TIRadarWindows import TIRadarWindow
 from HelpWindows import HelpWindow
 #from TOFWindows import TOFWindow
-#from IntelRealSenseWindows import IntelRealSenseWindow
+from IntelRealSenseWindows import IntelRealSenseWindow
 
 
 class MainWindow(QMainWindow):
 
         def __init__(self):
             super().__init__()
-            
-            self.setWindowTitle("Sensor Application")
-            self.setGeometry(50, 50, 1000, 800)
 
             self.application_values = AppValues()
             self.settings_window = SettingsWindow()
@@ -32,10 +30,8 @@ class MainWindow(QMainWindow):
             self.info_window = None
             self.help_window = HelpWindow()
          
-            self.connection_info_window = ConnectionInfoWindow() 
-            
-            self.createMenu()
-            self.createStatusBar()
+            self.connection_info_window = None 
+
             self.createGUI()
             self.connectGUI()
        
@@ -44,14 +40,10 @@ class MainWindow(QMainWindow):
                 self.menu_bar.move(0, 0)
                 self.menu_bar.setMaximumHeight(30)
                 self.device_menu = self.menu_bar.addMenu("&Connection/Device")
-                self.data_menu = self.menu_bar.addMenu("&Data")
+                #self.data_menu = self.menu_bar.addMenu("&Data")
                 self.help_menu = self.menu_bar.addMenu("&Help")
 
 
-                self.show_terminal = QAction("&Terminal window")
-                self.show_graphics = QAction("&Graphical window")
-                self.device_connection= QAction("&Connection")
-                self.device_info = QAction("&Device Info")
                 self.connection_info = QAction("&Connection Info")
 
                 self.intro_help = QAction("&Introduction")
@@ -59,7 +51,6 @@ class MainWindow(QMainWindow):
                 self.general_help = QAction("&Open Help")
                 self.about_help = QAction("&About")
 
-                self.device_menu.addAction(self.device_info)
                 self.device_menu.addAction(self.connection_info)
                 self.help_menu.addAction(self.intro_help)
                 self.help_menu.addAction(self.window_help)
@@ -72,15 +63,56 @@ class MainWindow(QMainWindow):
               self.status_bar = self.statusBar()
 
         def createGUI(self):
-                self.window_layout = MainWindowContent()
-                self.setCentralWidget(self.window_layout)
+                """
+                Function Creates GUI of Main Device Window
+                """
+                #self.showMaximized() 
+                self.central_widget = QWidget()       
+                self.layout = QVBoxLayout(self)
+                self.central_widget.setLayout(self.layout)
+                
+                        
+                self.setWindowTitle("Sensor Application")
+                self.setGeometry(50, 50, 1000, 800)
+
+                self.createMenu()
+                self.createStatusBar()
+                self.adjustGUI()
+                self.setCentralWidget(self.central_widget)
+
+                #Hiding this window by default (since it is not main window of the app but main window for device control)
+
+    
+        
+        
+        def adjustGUI(self):
+                self.label = QLabel("Sensor Application")
+                self.label.setFont(QFont('Times', 18))
+                #Settings Window Button            
+                self.settings_button = QPushButton("Settings")
+                self.settings_button.setFixedSize(200, 50)
+
+
+                #Sensor Window Button            
+                self.run_button = QPushButton("RUN")
+                self.run_button.setFixedSize(200, 50)
+
+
+                #Cancel Window Button
+                self.cancel_button = QPushButton("Cancel")
+                self.cancel_button.setFixedSize(200, 50)
+
+                #Adding elements to the layout
+                self.layout.addWidget(self.run_button, alignment = Qt.AlignCenter )
+                self.layout.addWidget(self.settings_button, alignment = Qt.AlignCenter)
+                self.layout.addWidget(self.cancel_button, alignment = Qt.AlignCenter )
 
         def connectGUI(self):
-               self.window_layout.run_button.clicked.connect(self.runDeviceWindow)
-               self.window_layout.settings_button.clicked.connect(self.runSettingsWindow)
+               self.run_button.clicked.connect(self.runDeviceWindow)
+               self.settings_button.clicked.connect(self.runSettingsWindow)
                self.settings_window.settings_signal.connect(self.application_values.receiveSettingsData)
                self.connection_info.triggered.connect(self.application_values.updateSettingsData) #connection similar to previous one in case no data are received by aplication_values from settings window
-               self.application_values.update_signal.connect(self.connection_info_window.createInfo) 
+               #self.application_values.update_signal.connect(self.connection_info_window.createInfo) 
                self.connection_info.triggered.connect(self.showConnectionInfoWindow)
                self.intro_help.triggered.connect(self.showHelpWindow)
                
@@ -111,15 +143,19 @@ class MainWindow(QMainWindow):
                                 self.sensor_window = TOFWindow()
                         elif settings_values["Device"] == "TI Radar":
                                 self.sensor_window = TIRadarWindow()
-                        elif settings_values["Device"] == ("Intel Real Sense L500" or "Intel Real Sense  D435 Dev 1" or "Intel Real Sense D435 Dev 2"):
-                                #self.sensor_window = IntelRealSenseWindow()
+                        elif settings_values["Device"] == "Intel Real Sense":
+                                self.sensor_window = IntelRealSenseWindow()
                                 print("Intel realsense")
+                        
+                        self.sensor_window.show()
+                
               
         def verify_connection(self, settings_data: dict):
                 device = settings_data["Device"]
                 port1 = settings_data["Port 1"]
                 port2 = settings_data["Port 2"]
-                specification = settings_data["Specification"]
+                specification = settings_data["Device specification"]
+                serial_number = settings_data["Serial number"]
 
                 verification_value = False
                 if device == "No device":
